8207858e62cc6d88d8a81a5797d4aa10c18bf15d
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-24 19:31:23 +0400

    Fix type annotations in sum_list and sum_mixed_list functions

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''Write a type-annotated function sum_list that takes a list of floats as argument
 and returns the sum of the floats as a float.'''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''Returns the sum of a list of floats'''
     return sum(input_list)
