6100c85efd352db1421b406d01a8d628e290e290
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-24 19:06:25 +0400

    Add type-annotated functions floor and to_str, and define annotated variables

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Write a type-annotated function floor that takes a float n as argument
+and returns the floor of the float as an integer.'''
+
+
+def floor(n: float) -> int:
+    '''Returns the floor of a float as an integer'''
+    return int(n)
---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Define variables with annotations: a is an integer with a value of 1,
+pi is a float with a value of 3.14,
+i_understand_annotations is a boolean with a value of True,
+and school is a string with the value Holberton.'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
