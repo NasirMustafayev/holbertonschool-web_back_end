628640858c3c719e6c214cd3bc9ffeeff54880d9
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-24 21:43:14 +0400

    Refactor element_length function to accept Iterable and Sequence types for improved flexibility

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 '''This module contains a type-annotated function element_length
 that takes a list of strings as argument and returns a list of tuples,
 where each tuple contains a string from the input list and its length.'''
-from typing import List, Tuple
+from typing import Iterable, List, Tuple, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Returns a list of tuples, where each tuple contains
     a string from the input list and its length'''
     return [(i, len(i)) for i in lst]
