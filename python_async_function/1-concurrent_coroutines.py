ae725b96a6eec76c61057e37e93a700b2ac53b1e
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-27 00:49:40 +0400

    Fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,13 +3,20 @@
 import asyncio
 import importlib
 from typing import List
-wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+
+wait_random = importlib.import_module(
+    '0-basic_async_syntax'
+).wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Returns a list of delays'''
+    '''Returns a sorted list of delays'''
     tasks = []
+
     for i in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         tasks.append(task)
-    return await asyncio.gather(*tasks)
+
+    delays = await asyncio.gather(*tasks)
+
+    return sorted(delays)
