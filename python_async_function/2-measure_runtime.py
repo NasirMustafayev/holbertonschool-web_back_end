cf3201cdef39411640da367e80a8ec76b2f3749e
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-27 03:01:57 +0400

    Fix adding max_delay arg

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,9 +8,9 @@ wait_n = importlib.import_module(
 ).wait_n
 
 
-async def measure_runtime(n: int) -> float:
+async def measure_runtime(n: int, max_delay: int) -> float:
     '''Measures the total runtime of wait_n(n)'''
     start_time = time.time()
-    await asyncio.gather(*[wait_n(i) for i in range(n)])
+    await asyncio.gather(*[wait_n(i, max_delay) for i in range(n)])
     end_time = time.time()
     return end_time - start_time
