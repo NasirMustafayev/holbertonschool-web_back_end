df03b3dcdab3ffc0966b76249c028b8fddf74eca
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-27 03:00:27 +0400

    Measuring the total run time

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''This module contains an async function measure_runtime'''
+import time
+import asyncio
+import importlib
+wait_n = importlib.import_module(
+    '1-concurrent_coroutines'
+).wait_n
+
+
+async def measure_runtime(n: int) -> float:
+    '''Measures the total runtime of wait_n(n)'''
+    start_time = time.time()
+    await asyncio.gather(*[wait_n(i) for i in range(n)])
+    end_time = time.time()
+    return end_time - start_time
