85e049bee8ddc2f35408eac60917a06b3c55e537
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-27 03:02:40 +0400

    fix function name

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = importlib.import_module(
 ).wait_n
 
 
-async def measure_runtime(n: int, max_delay: int) -> float:
+async def measure_time(n: int, max_delay: int) -> float:
     '''Measures the total runtime of wait_n(n)'''
     start_time = time.time()
     await asyncio.gather(*[wait_n(i, max_delay) for i in range(n)])
