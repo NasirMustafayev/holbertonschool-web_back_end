af353324108cc064f66e29fec494db90dace5c83
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-06-12 21:16:02 +0400

    Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+'''This module contains a simple pagination implementation.'''
+import csv
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset (i.e. the correct
+        list of rows).
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(self.dataset()):
+            return []
+
+        return self.dataset()[start_index:end_index]
