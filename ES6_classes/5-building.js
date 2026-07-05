1b4ab8ac4e5fca4f0f77be0b4d0582088fd487c9
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-07-05 17:18:10 +0400

    Task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+class Building{
+    constructor(sqft){
+        this._sqft = sqft
+
+        if(typeof this.evacuationWarningMessage !== 'function'){
+            throw new Error('Class extending Building must override evacuationWarningMessage')
+        }
+    }
+    get sqft(){
+        return this._sqft
+    }
+    set sqft(value){
+        if(typeof value !== 'number'){
+            throw new TypeError('Sqft must be number')
+        }
+        this._sqft = value
+    }
+}
+
