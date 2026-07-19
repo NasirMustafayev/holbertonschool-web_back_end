8fd6d8f237d1a9fee352aec944812c34ad84f33e
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-07-19 20:17:52 +0400

    Task 1

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+function getFullResponseFromAPI(success){
+    return new Promise((resolve, reject) =>{
+        if(success){
+            resolve({status:200,body:'Success'})
+        }
+        else{
+            reject(new Error('The fake API is not working currently'));
+        }
+    })
+}
+
+export default getFullResponseFromAPI
