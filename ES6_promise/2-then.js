ade513f4f00b94feba224c13ea727a8a8fbfb50d
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-07-20 01:34:44 +0400

    fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,13 @@
 function handleResponseFromAPI(promise) {
-  return promise.then((response) => {
-    console.log(response);
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
 
 export default handleResponseFromAPI;
