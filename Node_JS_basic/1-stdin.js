06479ab001f33de8288240a0fcce12dad79c6ddb
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-07-25 21:40:29 +0400

    fix lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,9 +5,9 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-    if (input !== null) {
-        process.stdout.write(`Your name is: ${input}`);
-    }
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
